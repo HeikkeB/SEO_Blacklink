7123e6600d1f118e5b42105e988168f5253cef8f
HeikkeB <vldbltn@gmail.com>
2023-03-10 20:35:15 +0300

    feat: make welcome content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
+import WelcomeContent from '../WelcomeContent/WelcomeContent';
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <WelcomeContent />
     <Promo />
     </div>
   );
--- src/components/WelcomeContent/WelcomeContent.jsx
+++ src/components/WelcomeContent/WelcomeContent.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './WelcomeContent.css'
+import square_green from '../../images/Menu img.svg'
+import search_img from '../../images/search_img.svg'
+import pc_image from '../../images/Pc_image.png'
 
 export default function WelcomeContent() {
   return (
-    <div className='welcome'> 
+    <div className='welcome'>
         <div className='welcome__top-block'>
             <div className='welcome__share'>
                 <h1 className='welcome__share-title'>SEO Backlink be Ranked 1</h1>
@@ -12,6 +17,12 @@ export default function WelcomeContent() {
                 we let you compare and book top local business -
                 Completely free.
                 </p>
+            <Link to='/' className='welcome__share-button'>EXPLORE OUR SERVICES</Link>
+            </div>
+            <div className='welcome__images'>
+              <img className='welcome__images-square' src={square_green} alt='square green' />
+              <img className='welcome__images-search' src={search_img} alt='search img' />
+              <img className='welcome__images-pc' src={pc_image} alt='pc img' />
             </div>
         </div>
         <div className='welcome__bottom-block'></div>
