d65ac9b8c9a14161acdc4849b66c8985254c2c18
HeikkeB <vldbltn@gmail.com>
2023-03-12 16:07:40 +0300

    feat: make footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Footer.css'
+import facebook from '../../images/facebook.svg'
+import twitter from '../../images/twitter.svg'
+import insta from '../../images/insta.svg'
+import linkid from '../../images/linkid.svg'
 
 export default function Footer() {
   return (
@@ -8,36 +12,66 @@ export default function Footer() {
         <ul className='footer__menu'>
             <ul className='footer__list'>
                 <h3 className='footer__list-title'>COMPANY</h3>
-                <li className='footer__list-link'>ABOUT</li>
-                <li className='footer__list-link'>CONTACT</li>
-                <li className='footer__list-link'>PRESS</li>
-                <li className='footer__list-link'>TEAM</li>
-                <li className='footer__list-link'>BLOG</li>
+                <div className='footer__list-title_line'></div>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>ABOUT</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>CONTACT</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>PRESS</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>TEAM</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>BLOG</Link>
+                </li>
             </ul>
             <ul className='footer__list'>
                 <h3 className='footer__list-title'>CUSTOMERS</h3>
-                <li className='footer__list-link'>SERVICES</li>
-                <li className='footer__list-link'>MY ACCOUNT</li>
-                <li className='footer__list-link'>SUPPORT</li>
-                <li className='footer__list-link'>FAQ</li>
+                <div className='footer__list-title_line'></div>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>SERVICE</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>MY ACCOUNT</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>SUPPORT</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>FAQ</Link>
+                </li>
             </ul>
             <ul className='footer__list'>
                 <h3 className='footer__list-title'>PROVIDERS</h3>
-                <li className='footer__list-link'>SERVICES</li>
-                <li className='footer__list-link'>CASE STUDIES</li>
-                <li className='footer__list-link'>SUPPORT</li>
-                <li className='footer__list-link'>FAQ</li>
+                <div className='footer__list-title_line'></div>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>SERVICES</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>CASE STUDIES</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>SUPPORT</Link>
+                </li>
+                <li className='footer__list-item'>
+                    <Link className='footer__list-link' to='/'>FAQ</Link>
+                </li>
             </ul>
             <ul className='footer__list'>
                 <h3 className='footer__list-title'>RECENT BLOG</h3>
-                <li className='footer__list-link'>
+                <div className='footer__list-title_line'></div>
+                <li className='footer__list-item'>
                     <p className='footer__list-text'>UX Design</p>
                     <p className='footer__list-text'>
                     Lorem ipsum is simply dumy texts is
                     widely used in the design.
                     <Link className='footer__list-text_link' to='/'>Read More</Link></p>  
                 </li>
-                <li className='footer__list-link'>
+                <li className='footer__list-item'>
                     <p className='footer__list-text'>UX Design</p>
                     <p className='footer__list-text'>
                     Lorem ipsum is simply dumy texts is
@@ -45,7 +79,34 @@ export default function Footer() {
                     <Link className='footer__list-text_link' to='/'>Read More</Link></p>  
                 </li>
             </ul>
-            <ul className='footer__list'></ul>
+            <ul className='footer__list'>
+            <h3 className='footer__list-title'>CUSTOMERS</h3>
+            <div className='footer__list-title_line'></div>
+            <li className='footer__list-item'>
+                <div className='footer__list-social'>
+                    <img className='footer__list-social-img' src={facebook} alt='social icon' />
+                    <h3 className='footer__list-social-title'>Facebook</h3>
+                </div>
+            </li>
+            <li className='footer__list-item'>
+                <div className='footer__list-social'>
+                    <img className='footer__list-social-img' src={twitter} alt='social icon' />
+                    <h3 className='footer__list-social-title'>Twitter</h3>
+                </div>
+            </li>
+            <li className='footer__list-item'>
+                <div className='footer__list-social'>
+                    <img className='footer__list-social-img' src={insta} alt='social icon' />
+                    <h3 className='footer__list-social-title'>Instagram</h3>
+                </div>
+            </li>
+            <li className='footer__list-item'>
+                <div className='footer__list-social'>
+                    <img className='footer__list-social-img' src={linkid} alt='social icon' />
+                    <h3 className='footer__list-social-title'>LinkedIn</h3>
+                </div>
+            </li>
+            </ul>
         </ul>
     </div>
   )
